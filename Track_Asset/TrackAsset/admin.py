a40edc6a480bbcd627d40a101d2e335938b8a59f
Asif520 <asifantraz@gmail.com>
2024-04-27 15:55:54 +0600

    3rd

--- Track_Asset/TrackAsset/admin.py
+++ Track_Asset/TrackAsset/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import User,Company,Company_Assets,Subscriber
+
+
+admin.site.register(User)
+admin.site.register(Company)
+admin.site.register(Company_Assets)
+admin.site.register(Subscriber)
--- Track_Asset/TrackAsset/models.py
+++ Track_Asset/TrackAsset/models.py
@@ -1,3 +1,40 @@
 from django.db import models
 
 # Create your models here.
+from django.contrib.auth.models import AbstractUser
+
+class User(AbstractUser):
+    name = models.CharField(max_length=200)
+    email = models.EmailField(unique=True)
+
+    USERNAME_FIELD = 'username'
+    REQUIRED_FIELDS = []
+
+
+class Subscriber(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
+
+
+
+class Company(models.Model):
+    name = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name
+
+class Company_Assets(models.Model):
+    company = models.ForeignKey(Company, on_delete = models.SET_NULL, null=True)
+    employee = models.CharField(max_length=200)
+    device = models.CharField(max_length=100,null=True, blank=True)
+    condition_choices = [
+        ('Good', 'Good'),
+        ('Fair', 'Fair'),
+        ('Poor', 'Poor'),
+    ]
+    checked_out = models.DateTimeField()
+    returned_in = models.DateTimeField(null=True, blank=True)
+    condition_when_checked_out = models.CharField(max_length=10, choices=condition_choices,null=True, blank=True)
+    condition_when_returned = models.CharField(max_length=10, choices=condition_choices, null=True, blank=True)
+
+
+
